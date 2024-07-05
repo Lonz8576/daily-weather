4b1fe871455d9a3e299ecc987a3cac4ce6bbe328
Lonz8576 <Lonz8576@users.noreply.github.com>
2024-07-05 15:01:41 -0400

    basic content
    content added, css started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-
+import sunny from './images/sun.png'
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      Weather app
+      <div className='container'>
+        <div className='weather-app'>
+             <div className='search-up'>
+              <i className='fa-solid fa-location-dot'></i>
+                    <div className='local'>London</div>
+                    </div>
+                    <div className='search-bar'>
+                      <input type='text' placeholder='Enter a location...' />
+                  <i className='fa-solid fa-magnifying-glass'></i>
+                    </div>
+
+            <div className='weather-icon'>
+              <img src={sunny} alt='sunny' />
+            </div>
+            <div className='date'>Friday, <strong> July 30,</strong> 2024</div>
+            <div className='weather-description'>Clear & Sunny</div>
+            <div className='temp'>30° C</div>
+
+            <div className='weather-details'>
+                <div className='humidity'>Humidity
+                  <div>
+              <i className='fa-solid fa-droplet'>80 %</i></div></div>
+
+                <div className='wind'>Wind
+                  <div>
+                <i className='fa-solid fa-wind'>16 km/h</i></div></div>
+            </div>
+
+        </div>
+        </div>
+
+      <footer>
+      This project was coded by Nic Alonso, opened sourced on {""}
+      <a href='https://github.com/Lonz8576' target='_blank'>GitHub</a>
+      </footer>
     </div>
+
   );
 }
 
