632cddea8f83adffb10677978bd44c887438bedd
Lonz8576 <Lonz8576@users.noreply.github.com>
2024-07-24 15:32:12 -0400

    forecast days created
    axios not updating for city change

--- src/ForecastData.js
+++ src/ForecastData.js
@@ -4,15 +4,18 @@ import WeatherIcon from "./WeatherIcon";
 
 
 
+
+
 export default function ForecastData(props) {
+
 function maxTemp(){
-    let dayMaxTemp = Math.round(props.futureData.temperature.day);
+    let dayMaxTemp = Math.round(props.data.temperature.day);
     return `${dayMaxTemp}°`;
 }
     
   
   function dailyDate() {
-    let date = new Date(props.futureData.time);
+    let date = new Date(props.data.time * 1000);
     let dailyDate = date.getDay();
     let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
@@ -25,7 +28,7 @@ function maxTemp(){
           <div className='day-one col'>
             
           <div className='col forecast-date'>{dailyDate()} 
-             <WeatherIcon icons={props.info.icons}  size={25} />
+          <div className="forecast-description">{props.data.condition.description} </div>
                 </div>
             </div>
             <div className='row d-flex justify-content-end temp-forecast'>
--- src/ForecastDays.js
+++ src/ForecastDays.js
@@ -7,40 +7,38 @@ import ForecastData from './ForecastData';
 
 
 
+
 export default function Forecast(props) {
   let [forecastReady, setForecastReady] = useState(false);
-  let [forecastDays, setForecastDays] = useState(null);
+  let [forecast, setForecast] = useState(null);
 
   function handleResponse(response) {
-    setTimeout(() => {
-  console.log("Delayed");
-}, "3000");
-      setForecastDays(response.data.daily);
+ 
+      setForecast(response.data.daily);
       setForecastReady(true);
 
   }
 
-
- 
-
-    if (forecastReady) {
-
+    if (forecastReady)   {
+      console.log(forecast);
       return (
 
       <div className='forecast-box'>
-      <div className='next d-flex justify-content-start'>Next 7 days</div>
-          <ForecastData futureData={forecastDays[0]} />
-          <ForecastData futureData={forecastDays[1]} />
-          <ForecastData futureData={forecastDays[2]} />
-          <ForecastData futureData={forecastDays[3]} />
-          <ForecastData futureData={forecastDays[4]} />
-          <ForecastData futureData={forecastDays[5]} />
-          <ForecastData futureData={forecastDays[6]} />
-         
+      <div className='next d-flex justify-content-start'>Next 7 days </div>
+          {forecast.map(function(nextDays, index) {
+            if (index < 6){
+              return (
+                <div key={index}>
+                <ForecastData data={nextDays}/>
+                  </div>
+              );
+            }
+          })}
+          <ForecastData data={forecast[0]} />
                 </div>
-      )
+                );
      
-     } else {
+   } else {
 
       const apiKey = "2e99ddt6a7e37f7c164ob09d070ab380";
       let lat = props.latlon.latitude;
@@ -59,8 +57,8 @@ export default function Forecast(props) {
           return "loading";
     
     
-       
-    }
-    }
+     }
+}
+    
   
 
